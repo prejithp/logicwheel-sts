3b8e3e510d99af3ad897c363f4f5e4080ed45793
Prejith Premkumar <prajithpremg@gmail.com>
2018-07-07 10:28:33 +0530

    added columns

--- bulma/app.js
+++ bulma/app.js
@@ -1,5 +1,10 @@
 $(function() {
+
+  //Click listener for expand button in dashboard messages
   $('.btn-expand').click(function(event) {
-    $(this).parent().next().toggleClass('hidden');
+    $msgBody = $(this).parent();
+    $msgBody.next().toggleClass('hidden');
+    console.log($(this).parent().parent());
+    $msgBody.parent().toggleClass('is-light is-info');
   });
 });
