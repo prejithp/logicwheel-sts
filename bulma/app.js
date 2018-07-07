2fbb2fce953cbfdb07fa0f78346272ae28d97eb2
Prejith Premkumar <prajithpremg@gmail.com>
2018-07-07 12:35:30 +0530

    make changes to ticket list plus jquery changes for dismiss button

--- bulma/app.js
+++ bulma/app.js
@@ -1,10 +1,23 @@
 $(function() {
 
-  //Click listener for expand button in dashboard messages
-  $('.btn-expand').click(function(event) {
-    $msgBody = $(this).parent();
+  //Click listener for dismiss button in dashboard messages. Also checks if all rows are hidden, in that case, the title preceding the table is also hidden
+  $('.btn-dismiss').click(function() {
+    var $msgRow = $(this).parents('tr');
+    $msgRow.hide();
+    var $msgTable = $msgRow.parents('table');
+    var $msgRows = $msgTable.find('tr');
+    var $hiddenMsgRows = $msgRows.children(':hidden');
+    
+    if($msgRows.length === $hiddenMsgRows.length) {
+      $msgTable.parent().prev().hide();
+      $msgTable.parent().hide();
+    }
+  })
+
+  //Click listener for expand button in dashboard messages. Toggles hidden class and sets background classes on clicks
+  $('.btn-expand').click(function() {
+    var $msgBody = $(this).parent();
     $msgBody.next().toggleClass('hidden');
-    console.log($(this).parent().parent());
     $msgBody.parent().toggleClass('is-light is-info');
   });
 });
