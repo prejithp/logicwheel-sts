50a83de82f8fec8617b65a4353601515fe559dff
Prejith Premkumar <prajithpremg@gmail.com>
2018-07-07 15:02:59 +0530

    write more jquery+new user form

--- bulma/app.js
+++ bulma/app.js
@@ -1,23 +1,33 @@
-$(function() {
+$(function () {
 
   //Click listener for dismiss button in dashboard messages. Also checks if all rows are hidden, in that case, the title preceding the table is also hidden
-  $('.btn-dismiss').click(function() {
+  $('.btn-dismiss').click(function () {
     var $msgRow = $(this).parents('tr');
     $msgRow.hide();
     var $msgTable = $msgRow.parents('table');
     var $msgRows = $msgTable.find('tr');
     var $hiddenMsgRows = $msgRows.children(':hidden');
-    
-    if($msgRows.length === $hiddenMsgRows.length) {
+
+    if ($msgRows.length === $hiddenMsgRows.length) {
       $msgTable.parent().prev().hide();
       $msgTable.parent().hide();
     }
   })
 
   //Click listener for expand button in dashboard messages. Toggles hidden class and sets background classes on clicks
-  $('.btn-expand').click(function() {
+  $('.btn-expand').click(function () {
     var $msgBody = $(this).parent();
     $msgBody.next().toggleClass('hidden');
     $msgBody.parent().toggleClass('is-light is-info');
+  });
+
+  $("#check-all").click(function () {
+    $(".checkbox").prop('checked', $(this).prop('checked'));
+  });
+
+  $(".checkbox").change(function () {
+    if (!$(this).prop("checked")) {
+      $("#check-all").prop("checked", false);
+    }
   });
 });
