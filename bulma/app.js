177590f2796d4951b60cf0043ea2c8a0738f0972
Prejith Premkumar <prajithpremg@gmail.com>
2018-07-08 20:29:46 +0530

    add edit user modal + open ticket and closed ticket screns

--- bulma/app.js
+++ bulma/app.js
@@ -1,5 +1,35 @@
 $(function () {
 
+  // ========== General functions ============
+
+  //Use for select all in modal boxes 
+  $("#check-all-modal").click(function () {
+    $(".checkbox-modal").prop('checked', $(this).prop('checked'));
+    console.log('click');
+
+  });
+
+  $(".checkbox-modal").change(function () {
+    if (!$(this).prop("checked")) {
+      $("#check-all-modal").prop("checked", false);
+    }
+  });
+
+  //Can be used in select all in main tables
+  $("#check-all").click(function () {
+    $(".checkbox").prop('checked', $(this).prop('checked'));
+    console.log('click');
+
+  });
+
+  $(".checkbox").change(function () {
+    if (!$(this).prop("checked")) {
+      $("#check-all").prop("checked", false);
+    }
+  });
+
+  //  =========== CLIENT DASHBOARD ============
+
   //Click listener for dismiss button in dashboard messages. Also checks if all rows are hidden, in that case, the title preceding the table is also hidden
   $('.btn-dismiss').click(function () {
     var $msgRow = $(this).parents('tr');
@@ -21,38 +51,23 @@ $(function () {
     $msgBody.parent().toggleClass('is-light is-info');
   });
 
+  // ========= USER LIST ============
+
   //listener for + icon in user list to open department selector
-  $('.modal-department').on('click', function() {
+  $('.modal-add-department').on('click', function () {
     $('#modal-department-root').addClass('is-active');
   });
 
-  $('#modal-department-close').click(function() {
+  $('#modal-department-close').click(function () {
     $('#modal-department-root').removeClass('is-active');
   });
 
-
-  //Use for select all in modal boxes 
-  $("#check-all-modal").click(function () {
-    $(".checkbox-modal").prop('checked', $(this).prop('checked'));
-    console.log('click');
-    
+  // listener for edit icon in user list to open edit user modal
+  $('.modal-edit-user').click(function() {    
+    $('#modal-edit-user-root').addClass('is-active');
   });
 
-  $(".checkbox-modal").change(function () {
-    if (!$(this).prop("checked")) {
-      $("#check-all-modal").prop("checked", false);
-    }
-  });
-  //Can be used in select all in main tables
-  $("#check-all").click(function () {
-    $(".checkbox").prop('checked', $(this).prop('checked'));
-    console.log('click');
-    
-  });
-
-  $(".checkbox").change(function () {
-    if (!$(this).prop("checked")) {
-      $("#check-all").prop("checked", false);
-    }
-  });
+  $('#modal-edit-user-close').click(function() {
+    $('#modal-edit-user-root').removeClass('is-active');
+  })
 });
